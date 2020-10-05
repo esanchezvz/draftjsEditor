040c27c0628956fd2f0ca890a188ea43f4d6d7c9
Esteban Sánchez <esteban.sanvaz@gmail.com>
2020-10-05 10:46:50 -0500

    added loading placeholder to tweet embed

--- src/components/Editor/AtomicBlock/Tweet.tsx
+++ src/components/Editor/AtomicBlock/Tweet.tsx
@@ -1,22 +1,36 @@
+import { useState } from 'react';
 import { Tweet } from 'react-twitter-widgets';
+import SyncLoader from 'react-spinners/SyncLoader';
 import Alert from '@material-ui/lab/Alert';
 import AlertTitle from '@material-ui/lab/AlertTitle';
 
 const EmbeddedTweet = ({ tweetId }: { tweetId: string }) => {
   // TODO -> Update theme dynamically
+  const [loading, setLoading] = useState(true);
 
   return (
-    <Tweet
-      // ref={tweetRef}
-      options={{ align: 'center', theme: 'light' }}
-      tweetId={tweetId}
-      renderError={(_e) => (
-        <Alert severity='error'>
-          <AlertTitle>Error</AlertTitle>
-          Error al cargar el tweet. <strong>Revisa que el url sea correcto.</strong>
-        </Alert>
-      )}
-    />
+    <>
+      {loading && <LoadingPlaceHolder loading={loading} />}
+      <Tweet
+        onLoad={() => setLoading(false)}
+        options={{ align: 'center', theme: 'light' }}
+        tweetId={tweetId}
+        renderError={(_e) => (
+          <Alert severity='error'>
+            <AlertTitle>Error</AlertTitle>
+            Error al cargar el tweet. <strong>Revisa que el url sea correcto.</strong>
+          </Alert>
+        )}
+      />
+    </>
+  );
+};
+
+const LoadingPlaceHolder = ({ loading }: { loading: boolean }) => {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <SyncLoader color='#F7F7F7' loading={loading} />
+    </div>
   );
 };
 
